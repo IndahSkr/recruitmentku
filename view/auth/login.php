76bcb768d5169af74cd3ad48facba7ad5bf6dae4
Arif Hayati Indah Lestari <indahlestari1004@gmail.com>
2024-08-31 22:38:02 +0700

    create login

---
+++ view/auth/login.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Merekrutmu</title>
+  <link rel="shortcut icon" type="image/png" href="../../assets/images/logos/favicon.png">
+  <link rel="stylesheet" href="../../assets/css/styles2.css" />
+</head>
+
+<body>
+  <div id="main-wrapper" class="auth-customizer-none">
+    <div class="position-relative overflow-hidden radial-gradient min-vh-100 w-100">
+      <div class="position-relative z-index-5">
+        <div class="row">
+          <div class="col-xl-7 col-xxl-8">
+            <a href="../main/index.html" class="text-nowrap logo-img d-block px-4 py-9 w-100">
+              <img src="../../assets/images/logos/dark-logo.svg" class="dark-logo" alt="Logo-Dark" />
+            </a>
+            <div class="d-none d-xl-flex align-items-center justify-content-center h-n80">
+              <img src="../../assets/images/backgrounds/login-security.svg" alt="modernize-img" class="img-fluid" width="500">
+            </div>
+          </div>
+          <div class="col-xl-5 col-xxl-4">
+            <div class="authentication-login min-vh-100 bg-body row justify-content-center align-items-center ">
+              <div class="auth-max-width col-sm-8 col-md-6 col-xl-7 px-4">
+                <h2 class="mb-1 fs-7 fw-bolder">Merekrutmu</h2>
+                <p class="mb-7">Selamat datang</p>
+                <div class="row">
+                  <div class="col-12 mb-2 mb-sm-0">
+                    <a class="btn text-dark border fw-normal d-flex align-items-center justify-content-center rounded-2 py-8" href="javascript:void(0)" role="button">
+                      <img src="../../assets/vendor/icons/icons/outline/brand-chrome.svg" alt="modernize-img" class="img-fluid me-2" width="18" height="18">
+                      <span class="flex-shrink-0">Google</span>
+                    </a>
+                  </div>
+                  <!-- <div class="col-6">
+                    <a class="btn text-dark border fw-normal d-flex align-items-center justify-content-center rounded-2 py-8" href="javascript:void(0)" role="button">
+                      <img src="../../assets/images/svgs/facebook-icon.svg" alt="modernize-img" class="img-fluid me-2" width="18" height="18">
+                      <span class="flex-shrink-0">with FB</span>
+                    </a>
+                  </div> -->
+                </div>
+                <div class="position-relative text-center my-4">
+                  <p class="mb-0 fs-4 px-3 d-inline-block bg-body text-dark z-index-5 position-relative">
+                    Atau masuk
+                  </p>
+                  <span class="border-top w-100 position-absolute top-50 start-50 translate-middle"></span>
+                </div>
+                <form>
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Username</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                  </div>
+                  <div class="mb-4">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                  </div>
+                  <div class="d-flex align-items-center justify-content-between mb-4">
+                    <!-- <div class="form-check">
+                      <input class="form-check-input primary" type="checkbox" value="" id="flexCheckChecked" checked>
+                      <label class="form-check-label text-dark fs-3" for="flexCheckChecked">
+                        Remeber this Device
+                      </label>
+                    </div> -->
+                    <a class="text-primary fw-medium fs-3" href="#">Lupa Password?</a>
+                  </div>
+                  <a href="../menu/dashboard/index.php" class="btn btn-primary w-100 py-8 mb-4 rounded-2">Masuk</a>
+                  <div class="d-flex align-items-center justify-content-center">
+                    <p class="fs-4 mb-0 fw-medium">Belum memiliki akun?</p>
+                    <a class="text-primary fw-medium ms-2" href="../main/authentication-register.html">Buat akun</a>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="../../assets/vendor/jquery/dist/jquery.min.js"></script>
+    <script src="../../assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- <script src="../../assets/js/sidebarmenu.js"></script>
+    <script src="../../assets/js/app.min.js"></script> -->
+    <!-- <script src="../../assets/libs/apexcharts/dist/apexcharts.min.js"></script>
+    <script src="../../assets/libs/simplebar/dist/simplebar.js"></script> -->
+    <!-- <script src="../../assets/js/dashboard.js"></script> -->
+</body>
+
+</html>
