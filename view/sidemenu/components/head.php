e76b7a9cf97d4cc7540efab262687d21f5de3150
Arif Hayati Indah Lestari <indahlestari1004@gmail.com>
2024-08-31 22:38:30 +0700

    create head template

---
+++ view/sidemenu/components/head.php
@@ -0,0 +1,5 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Merekrutmu</title>
+<link rel="shortcut icon" type="image/png" href="../../../assets/images/logos/favicon.png">
+<link rel="stylesheet" href="../../../assets/css/styles2.css" />
