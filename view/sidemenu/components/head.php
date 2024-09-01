18888d16e8012e1707127f44d0db9b2a1bfaaaf3
Arif Hayati Indah Lestari <indahlestari1004@gmail.com>
2024-09-01 21:50:07 +0700

    update head

--- view/sidemenu/components/head.php
+++ view/sidemenu/components/head.php
@@ -2,4 +2,4 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Merekrutmu</title>
 <link rel="shortcut icon" type="image/png" href="../../../assets/images/logos/favicon.png">
-<link rel="stylesheet" href="../../../assets/css/styles2.css" />
+<link rel="stylesheet" href="../../../assets/css/styles.css" />
