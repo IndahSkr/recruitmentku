a8dec36e0a3c3d8003bad8f627953341afc828d0
Arif Hayati Indah Lestari <indahlestari1004@gmail.com>
2024-09-08 22:07:33 +0700

    create login bridging

---
+++ model/bridge/auth/login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../../../assets/vendor/sweetalert2/dist/sweetalert2.all.min.js"></script>
+    <link rel="stylesheet" href="../../../assets/vendor/sweetalert2/dist/sweetalert2.min.css">
+</head>
+
+<body>
+    <?php
+    // echo $_POST['iptNmlkp'];
+    session_start();
+    include "../../url/alamat.php";
+    include "../../../assets/vendor/func/curl.php";
+
+    $word = $_GET['word'];
+
+    if ($word == "login") {
+        $uname = $_POST['iptUname'];
+        $pw = $_POST['iptPw'];
+
+        $dt = array(
+            "code" => "login",
+            "uname" => $uname,
+            "pw" => $pw
+        );
+
+        $dtjson = json_encode($dt);
+        $send = curlpost($url, $dtjson);
+        $result = json_decode($send, TRUE);
+        // $hasil = $result['status'];
+        print_r($send);
+    }
+    ?>
+</body>
+
+</html>
