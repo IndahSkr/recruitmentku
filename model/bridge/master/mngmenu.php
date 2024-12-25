cd53c2600db79fb2549d0e5075c27526bf18c3d1
Arif Hayati Indah Lestari <indahlestari1004@gmail.com>
2024-12-25 21:38:01 +0700

    create bridge management menu

---
+++ model/bridge/master/mngmenu.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Management</title>
+    <script src="../../../assets/vendor/sweetalert2/dist/sweetalert2.all.min.js"></script>
+    <link rel="stylesheet" href="../../../assets/vendor/sweetalert2/dist/sweetalert2.min.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
